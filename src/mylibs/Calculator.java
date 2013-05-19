f52eaee7b403313508f97b5510add607e98508f3
abhishek_nair <abhishek.alchemist@gmail.com>
2013-05-19 18:03:20 +0530

    add calculator lib

---
+++ src/mylibs/Calculator.java
@@ -0,0 +1,16 @@
+package mylibs;
+
+//class is a library for calaulator functions
+
+public class Calculator {
+	
+	public static int add(int x, int y) {
+		//add x and y
+		return x+y;
+	}
+	
+	public static int sub(int x, int y) {
+		//subtract y from x
+		return x-y;
+	}
+}
--- src/mypack/MainClass.java
+++ src/mypack/MainClass.java
@@ -1,5 +1,7 @@
 package mypack;
 
+import mylibs.Calculator;
+
 public class MainClass {
 
 	/**
@@ -8,6 +10,9 @@ public class MainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("this is just a test");
+		int res=0, x = 10, y = 5;
+		res = Calculator.sub(x, y);
+		System.out.println("result is: "+res);
 	}
 
 }
