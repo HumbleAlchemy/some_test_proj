f03a6a4e958e50e71c3ba33238b505b2c35baa92
abhishek_nair <abhishek.alchemist@gmail.com>
2013-05-19 18:44:02 +0530

    add final feature to Numbers.java, code complete

--- src/mylibs/Numbers.java
+++ src/mylibs/Numbers.java
@@ -2,4 +2,8 @@ Class Numbers {
 	public static void changeFormat() {
 		System.out.println("decimal to hex");
 	}
+
+	public static void changeFormatOct() {
+		System.out.println("decimal to oct");
+	}
 }
